43ff7819236c1610f37b2467dbe5abc16e1c4add
Gabriel Knupp <gabrielknpp@gmail.com>
2022-10-19 15:42:44 -0300

    criada a cobrinha do game

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,24 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+let snake = [];
+
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16*box, 16*box);
 }
 
+function criarCobra() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "darkgreen";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobra();
